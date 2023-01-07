7b6b5573488b71712ddd8be3fc6835389d810f9b
Роман Худенко <damage----------inc@yandex.ru>
2023-01-07 10:58:20 +0400

    Added database variables in build.gradle, slightly changed db logic.

--- src/test/java/ru/netology/data/DBHelper.java
+++ src/test/java/ru/netology/data/DBHelper.java
@@ -5,6 +5,10 @@ import java.sql.SQLException;
 
 public class DBHelper {
 
+    private static final String db_link = System.getProperty("db_link");
+    private static final String db_username = System.getProperty("db_username");
+    private static final String db_password = System.getProperty("db_password");
+
     private static final String orderSQL = "SELECT payment_id FROM order_entity ORDER BY created DESC LIMIT 1";
     private static final String paymentStatusSQL = "SELECT status FROM payment_entity WHERE transaction_id = ? LIMIT 1";
     private static final String creditRequestStatusSQL = "SELECT status FROM credit_request_entity WHERE bank_id = ? LIMIT 1";
@@ -13,7 +17,7 @@ public class DBHelper {
         String output = null;
         try (
                 var conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/app", "petya", "qwerty123"
+                        db_link, db_username, db_password
                 );
                 var statement = conn.prepareStatement(orderSQL)
         ) {
@@ -32,7 +36,7 @@ public class DBHelper {
         var status = "";
         try (
                 var conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/app", "petya", "qwerty123"
+                        db_link, db_username, db_password
                 );
                 var statement = conn.prepareStatement(paymentStatusSQL)
         ) {
@@ -52,7 +56,7 @@ public class DBHelper {
         var status = "";
         try (
                 var conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/app", "petya", "qwerty123"
+                        db_link, db_username, db_password
                 );
                 var statement = conn.prepareStatement(creditRequestStatusSQL)
         ) {
